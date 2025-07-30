243d224bff813b7e1c2e2f087ad843ffc442c760
PranadityaTriJyotistaVavitramPS <gedearisudana16@gmail.com>
2025-07-30 20:29:16 +0700

    lupa log out hehe

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { updateUser } from '../utils/network-data';
+import { updateUser, logout } from '../utils/network-data';
+import { useNavigate } from 'react-router-dom';
 
 export default function Profile({ user, onSaveProfile }) {
   const [username, setUsername] = useState(user.username);
@@ -7,6 +8,12 @@ export default function Profile({ user, onSaveProfile }) {
   const [currentPassword, setCurrentPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    logout();
+    navigate('/')
+  }
   
 
   const handleProfileSubmit = e => {
@@ -89,6 +96,8 @@ export default function Profile({ user, onSaveProfile }) {
         />
 
         <button type="submit">Ganti Password</button>
+
+        <button className='log-out' onClick={handleLogOut}>Log Out</button>
       </form>
     </div>
   );
